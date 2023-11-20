d85e4d8262bfcf05464509affb46e21b979e358e
Malin Patriksson <malinpatriksson@hotmail.com>
2023-11-20 17:12:54 +0100

    constructor added TestKlass2

--- src/TestKlass2.java
+++ src/TestKlass2.java
@@ -2,4 +2,28 @@ public class TestKlass2 {
     //blabla
     //1234
     // Ny rad
+
+    String namn;
+    int ålder;
+
+    public TestKlass2(String namn, int ålder) {
+        this.namn = namn;
+        this.ålder = ålder;
+    }
+
+    public String getNamn() {
+        return namn;
+    }
+
+    public void setNamn(String namn) {
+        this.namn = namn;
+    }
+
+    public int getÅlder() {
+        return ålder;
+    }
+
+    public void setÅlder(int ålder) {
+        this.ålder = ålder;
+    }
 }
